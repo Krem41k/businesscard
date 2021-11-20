1d04f9a2178802b38fe10f23d5c506298b59ef45
Daniil Kremyansky <densca@mail.ru>
2021-11-20 23:50:57 +0700

    Добавление минимального кэша

--- src/main/java/com/softlab/businesscard/controllers/BusinessCardController.java
+++ src/main/java/com/softlab/businesscard/controllers/BusinessCardController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.Cacheable;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
