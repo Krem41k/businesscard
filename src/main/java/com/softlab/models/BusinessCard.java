649ea4db14899b5691379d5bae118087f453e7a8
Daniil Kremyansky <densca@mail.ru>
2021-11-20 13:12:20 +0700

    Добавление модели и репозитория

---
+++ src/main/java/com/softlab/models/BusinessCard.java
@@ -0,0 +1,108 @@
+package com.softlab.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class BusinessCard {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String surname;
+    private String patronymic;
+    private int age;
+    private String city;
+    private String phone;
+    private String email;
+    private String aboutMe;
+
+    public BusinessCard() {
+    }
+
+    public BusinessCard(String name, String surname, String patronymic, int age, String city,
+                        String phone, String email, String aboutMe) {
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.age = age;
+        this.city = city;
+        this.phone = phone;
+        this.email = email;
+        this.aboutMe = aboutMe;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAboutMe() {
+        return aboutMe;
+    }
+
+    public void setAboutMe(String aboutMe) {
+        this.aboutMe = aboutMe;
+    }
+}
---
+++ src/main/java/com/softlab/repository/BusinessCardRepository.java
@@ -0,0 +1,8 @@
+package com.softlab.repository;
+
+import com.softlab.models.BusinessCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class BusinessCardRepository extends JpaRepository<BusinessCard, Long> {
+
+}
